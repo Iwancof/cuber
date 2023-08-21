8e6baee89c3979093b9627bbd6de72bf41cc6f3c
Iwancof <rock.iwanof@gmail.com>
2023-08-21 15:20:21 +0900

    add rust project

---
+++ cuber/target/debug/deps/cuber-1193e520f0489abc.d
@@ -0,0 +1,5 @@
+/home/iwancof/Nextcloud/Rust/cuber/cuber/target/debug/deps/cuber-1193e520f0489abc.rmeta: src/main.rs
+
+/home/iwancof/Nextcloud/Rust/cuber/cuber/target/debug/deps/cuber-1193e520f0489abc.d: src/main.rs
+
+src/main.rs:
---
+++ cuber/target/debug/deps/cuber-64935c1ff3a503e3.d
@@ -0,0 +1,5 @@
+/home/iwancof/Nextcloud/Rust/cuber/cuber/target/debug/deps/cuber-64935c1ff3a503e3.rmeta: src/main.rs
+
+/home/iwancof/Nextcloud/Rust/cuber/cuber/target/debug/deps/cuber-64935c1ff3a503e3.d: src/main.rs
+
+src/main.rs:
